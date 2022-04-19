d89dd3e9c2f8ff5d6ef8d276a79dddca8f55b390
HatemGr <hatemgribi@gmail.com>
2022-04-19 15:01:37 +0200

    HG: Modification de la methode sum3/5

--- lib/exo.rb
+++ lib/exo.rb
@@ -1,23 +1,19 @@
 # Multiple 3 et 5 
 
-def sum_of_3_or_5_multiples(num)
-    if num.is_a? Integer
-        if num>= 0
-            sum = 0
-            for i in (1..num - 1) 
-                is_multiple_of_3_or_5(i) ? sum += i : sum
-            end
-            return sum
-        else
-            return "Je ne prends que des entiers naturels"
-        end
-        else
-            return "Je ne prends que des entiers naturels"
+def sum_of_3_or_5_multiples(final_number)
+    final_sum = 0 
+    if final_number.to_s.to_i != final_number || final_number < 0
+      return "Je ne prends que des entiers naturels"
     end
-end
+  
+    for current_number in (0..final_number-1)
+      if is_multiple_of_3_or_5(current_number)
+        final_sum += current_number
+      end
+    end 
+    return final_sum 
+  end
 
 def is_multiple_of_3_or_5(num)
     num%3 == 0 || num%5 ==0
 end
-
-puts "abc".is_a? Integer
